38b33b3e57aa94c9a199195b440cbd01909c019c
Edem Ahadzi <ahadzi.airdem@gmail.com>
2022-02-27 07:48:03 +0000

    feat: fap-21 implement constituency equipment form, page 32

--- web-react/src/pages/campaigns/CampaignQueries.js
+++ web-react/src/pages/campaigns/CampaignQueries.js
@@ -12,3 +12,20 @@ export const CONSTITUENCY_CAMPAIGN_LIST = gql`
     }
   }
 `;
+
+export const CONSTITUENCY_EQUIPMENT_RECORD_CREATION = gql`
+  mutation CreateConstituencyEquipmentRecord(
+    $constituencyId: ID!
+    $pulpits: Int!
+    $date: Date!
+  ) {
+    CreateConstituencyEquipmentRecord(
+      constituencyId: $constituencyId
+      pulpits: $pulpits
+      date: $date
+    ) {
+      id
+      pulpits
+    }
+  }
+`;
