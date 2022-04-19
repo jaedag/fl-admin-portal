4180a70d51f2d64fef37c1fe7ebe96606a8157b0
David Dag Vanderpuije <dabick14@gmail.com>
2022-04-19 11:17:21 +0000

    feat: adding resolvers for defaulters and creating campaigns

--- api/src/resolvers/campaigns.js
+++ api/src/resolvers/campaigns.js
@@ -1,3 +1,14 @@
 //import { permitAdmin } from './permissions'
 
-export const campaignsMutations = {}
+import { isAuth } from './resolver-utils'
+import { permitAdmin } from './permissions'
+
+export const campaignsMutations = {
+  Mutattion: {
+    CreateCampaigns: async (object, args, context) => {
+      isAuth(permitAdmin('Fellowship'), context.auth.roles)
+
+      //const session = context.driver.session()
+    },
+  },
+}
