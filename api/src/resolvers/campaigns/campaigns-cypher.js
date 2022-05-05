d2052ffc436e59fc7547c3fc85465ff33862a531
David Dag Vanderpuije <dabick14@gmail.com>
2022-05-05 15:50:22 +0000

    fix: create equipment campaign, churchType prodcuing three records

--- api/src/resolvers/campaigns/campaigns-cypher.js
+++ api/src/resolvers/campaigns/campaigns-cypher.js
@@ -3,11 +3,10 @@ MATCH (target:Target {campaign:"Equipment"})
 MATCH (church {id:$id}) WHERE church:Bacenta OR church:Constituency OR church:Council OR church:Stream OR church:GatheringService OR church:Fellowship
 MATCH (log:HistoryLog)<-[:HAS_HISTORY {current:true}]-(church)
 MATCH (leader:Member)-[:LEADS]->(church)
-UNWIND labels(church) as churchType
 
-WITH target, church, log, leader, churchType
+WITH target, church, log, leader
 CREATE (churchCampaign:EquipmentCampaign {id: apoc.create.uuid()})
-SET churchCampaign.name = church.name + ' '+churchType
+SET churchCampaign.name = church.name 
 MERGE (church)-[:HAS_CAMPAIGN]->(churchCampaign)
 MERGE (churchCampaign)<-[:LEADS]-(leader)
 MERGE (churchCampaign)-[:HAS_HISTORY]->(log)
