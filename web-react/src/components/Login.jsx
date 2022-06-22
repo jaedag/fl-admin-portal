c2eb4a8eda041f6330b66cb23e976fd1b6601058
Edem Ahadzi <ahadzi.airdem@gmail.com>
2022-06-22 23:12:31 +0000

    feat: implement new login screen

--- web-react/src/components/Login.jsx
+++ web-react/src/components/Login.jsx
@@ -5,30 +5,62 @@ import MobileView from './responsive-design/MobileView'
 import TabletDesktopView from './responsive-design/TabletDesktopView'
 import { Container } from 'react-bootstrap'
 import './Login.css'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import BarIcon from '../assets/bars.svg'
+import GlobeIcon from '../assets/globe.svg'
 
 const Login = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+  }
   const catchPhrase = 'A church full of young people on fire for the Lord'
 
   return (
     <>
       <TabletDesktopView>
-        <div className="container  ">
-          {/* <!--Web Logo and text--> */}
+        <div className="container text-center mt-5 desktop-card">
           <div className="row align-self-center">
-            {/* <!--Sign In--> */}
-
-            <form className="login-page-lg">
-              <div className="m-5">
-                <div className="col-auto my-3">
-                  <img src={Logo} alt="logo" className="img-fluid" />{' '}
-                  <div className="mb-3">{catchPhrase}</div>
-                  <p className="text-secondary mb-3">
-                    Click to log in to your servants portal
-                  </p>
-                </div>
-                <AuthButton />
+            <Slider {...settings} className="mb-5 mt-5">
+              <div>
+                <img
+                  src={Logo}
+                  alt="logo"
+                  className="img-fluid mx-auto d-block logo"
+                />
               </div>
-            </form>
+              <div>
+                <img
+                  src={BarIcon}
+                  alt="bar"
+                  className="img-fluid mx-auto d-block logo"
+                />
+              </div>
+              <div>
+                <img
+                  src={GlobeIcon}
+                  alt="globe"
+                  className="img-fluid mx-auto d-block logo"
+                />
+              </div>
+            </Slider>
+            <p className="mb-3 mt-3 text-white">FLC SERVANTS PORTAL</p>
+
+            <p className="mt-3 text-white">{catchPhrase}</p>
+
+            <p className="text-secondary mb-3">
+              Click to log in to your servants portal
+            </p>
+            <div className="col-6 mx-auto mb-5">
+              <AuthButton mobileFullSize="true" />
+            </div>
           </div>
         </div>
       </TabletDesktopView>
@@ -36,27 +68,38 @@ const Login = () => {
       {/* <!--Mobile--> */}
       <MobileView className="bg-wrapper">
         <Container className="text-center mt-5 bg-content">
-          <img
-            src={Logo}
-            alt="logo"
-            className="img-fluid mx-auto d-block logo"
-          />
+          <Slider {...settings} className="mb-5">
+            <div>
+              <img
+                src={Logo}
+                alt="logo"
+                className="img-fluid mx-auto d-block logo"
+              />
+            </div>
+            <div>
+              <img
+                src={BarIcon}
+                alt="bar"
+                className="img-fluid mx-auto d-block logo"
+              />
+            </div>
+            <div>
+              <img
+                src={GlobeIcon}
+                alt="globe"
+                className="img-fluid mx-auto d-block logo"
+              />
+            </div>
+          </Slider>
+          <p className="mb-3 mt-3 text-white">FLC SERVANTS PORTAL</p>
 
-          <div className="mb-5 font-primary">
-            <p>
-              <i>{catchPhrase}</i>
-            </p>
-          </div>
+          <p className="mt-3 text-white">{catchPhrase}</p>
 
           <p className="text-secondary mb-3">
             Click to log in to your servants portal
           </p>
-          <div className="col-auto text-center">
+          <div className="col-6 mx-auto ">
             <AuthButton mobileFullSize="true" />
-          </div>
-
-          <div className="col-12 col-lg-6 d-flex justify-content-center my-3 ">
-            <div className=" flex-grow-1" />
           </div>
         </Container>
       </MobileView>
