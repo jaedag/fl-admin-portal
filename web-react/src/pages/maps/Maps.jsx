bb0f43d731b999a66bd3bc5b977ecad2897b48e9
John-Dag Addy <jaedagy@gmail.com>
2022-06-28 16:39:55 +0000

    feat: admin-171 implemented a map that loads with First Love Center in view

--- web-react/src/pages/maps/Maps.jsx
+++ web-react/src/pages/maps/Maps.jsx
@@ -1,12 +1,33 @@
 import { HeadingPrimary } from 'components/HeadingPrimary/HeadingPrimary'
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { useLoadScript } from '@react-google-maps/api'
+import Map from './Map'
+import { useState } from 'react'
 
 const Maps = () => {
+  const [libraries] = useState(['places'])
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  })
+  if (isLoaded) {
+    return (
+      <Container>
+        <HeadingPrimary>Maps</HeadingPrimary>
+        The Maps Feature is still being worked on. Will update soon!
+      </Container>
+    )
+  }
+
+  if (!isLoaded) {
+    return <div>Loading...</div>
+  }
+
   return (
     <Container>
       <HeadingPrimary>Maps</HeadingPrimary>
-      The Maps Feature is still being worked on. Will update soon!
+      <Map />
     </Container>
   )
 }
