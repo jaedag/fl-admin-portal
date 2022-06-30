2a3661710d09a8185966a805546774de0efb4b9b
David Dag Vanderpuije <dabick14@gmail.com>
2022-06-30 18:51:05 +0000

    fix: changed property name to tellerConfirmationTime and added sunday to defaulters list of days

--- api/src/resolvers/anagkazo/treasury-cypher.js
+++ api/src/resolvers/anagkazo/treasury-cypher.js
@@ -1,13 +1,13 @@
 export const confirmBanking = `
 MATCH (record:ServiceRecord {id:$serviceRecordId})
 MATCH  (teller:Member {auth_id: $auth.jwt.sub})
-SET record.tellerConfirmationAt = datetime()
+SET record.tellerConfirmationTime = datetime()
 MERGE (teller)-[:CONFIRMED_BANKING_FOR]->(record)
 RETURN record
 `
 
 export const checkIfConfirmed = `
 MATCH (record:ServiceRecord {id:$serviceRecordId})
-WHERE record.tellerConfirmationAt IS NOT NULL
+WHERE record.tellerConfirmationTime IS NOT NULL
 RETURN true
 `
