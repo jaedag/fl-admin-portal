b231532fa3cd0470c1a8afd8abfdd416088f5c64
John-Dag Addy <jaedagy@gmail.com>
2022-07-06 08:47:33 +0000

    fix: updated api deps and increased icon sizes

--- web-react-ts/src/assets/icons/FellowshipIcon.tsx
+++ web-react-ts/src/assets/icons/FellowshipIcon.tsx
@@ -4,8 +4,6 @@ const FellowshipIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
       fill="green"
       className="bi bi-people-fill"
       viewBox="0 0 16 16"
