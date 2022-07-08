5bf67b18dbb1a63378a5c78ecf6134eca09dcd9f
John-Dag Addy <jaedagy@gmail.com>
2022-07-08 21:26:36 +0000

    refactor: reorganised resolvers import order

--- api/src/resolvers/resolvers.ts
+++ api/src/resolvers/resolvers.ts
@@ -1,12 +1,14 @@
-// import { arrivalsMutation } from './arrivals/arrivals-resolvers'
-// import { bankingMutation } from './banking/banking-resolver'
+import componentResolvers from 'aggregates/component-resolvers'
 import MakeServantResolvers from './directory/make-servant-resolvers'
 import serviceNoIncomeMutations from './no-income/service-resolvers'
 import serviceMutation from './services/service-resolvers'
 import { Member } from './utils/types'
 import treasuryMutations from './anagkazo/treasury-resolvers'
 import directoryMutation from './directory/directory-resolvers'
-import { arrivalsMutation } from './arrivals/arrivals-resolvers'
+import {
+  arrivalsMutation,
+  arrivalsResolvers,
+} from './arrivals/arrivals-resolvers'
 import bankingMutation from './banking/banking-resolver'
 
 const resolvers = {
@@ -15,9 +17,26 @@ const resolvers = {
   // Args: Field Arguments
   // Context: Context object, database connection, API, etc
   // GraphQLResolveInfo
+
   Member: {
     fullName: (source: Member) => `${source.firstName} ${source.lastName}`,
   },
+  Bacenta: {
+    ...componentResolvers.Bacenta,
+  },
+  Constituency: {
+    ...componentResolvers.Constituency,
+  },
+  Council: {
+    ...componentResolvers.Council,
+  },
+  Stream: {
+    ...componentResolvers.Stream,
+  },
+  GatheringService: {
+    ...componentResolvers.GatheringService,
+  },
+  ...arrivalsResolvers,
   Mutation: {
     ...MakeServantResolvers,
     ...directoryMutation,
