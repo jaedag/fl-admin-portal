fbc8280aaa0c09935cbc21f607414740e58ca83f
Edem Ahadzi <ahadzi.airdem@gmail.com>
2022-07-20 16:16:09 +0000

    refactor: remove midweek banking home and change midweek banking button to add treasurers

--- web-react-ts/src/pages/services/banking/anagkazo/anagkazoBankingRoutes.ts
+++ web-react-ts/src/pages/services/banking/anagkazo/anagkazoBankingRoutes.ts
@@ -1,15 +1,8 @@
 import { RouteTypes } from 'global-types'
-import MidweekBankingHome from './MidweekBankingHome'
 import ConfirmAnagkazoBanking from './ConfirmAnagkazoBanking'
 import TreasurerSelect from './TellerSelect'
 
 export const anagkazoRoutes: RouteTypes[] = [
-  {
-    path: '/services/stream/midweek-banking',
-    element: MidweekBankingHome,
-    roles: ['adminStream'],
-    placeholder: false,
-  },
   {
     path: '/anagkazo/treasurer-select',
     element: TreasurerSelect,
