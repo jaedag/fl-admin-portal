6c6264a11211fff1f29d907fcec35ab3787fe42c
Godwin Adebimpe <londonfldata@gmail.com>
2022-07-28 14:13:40 +0100

    feat: made changes on review

--- web-react-ts/src/pages/directory/grids/OversightMembers.tsx
+++ web-react-ts/src/pages/directory/grids/OversightMembers.tsx
@@ -3,7 +3,6 @@ import { useQuery } from '@apollo/client'
 import MembersGrid from '../../../components/members-grids/MembersGrid'
 import { GET_OVERSIGHT_MEMBERS } from './GridQueries'
 import { ChurchContext } from '../../../contexts/ChurchContext'
-// import TabletDesktopView from 'components/responsive-design/TabletDesktopView'
 
 const OversightMembers = () => {
   const { oversightId } = useContext(ChurchContext)
