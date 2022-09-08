b75187334988e6951e5473fbf2ae47904ba5c3d2
Edem Ahadzi <ahadzi.airdem@gmail.com>
2022-09-08 08:26:49 +0000

    feat: implement equipment deadline on the equioment main page

--- web-react-ts/src/pages/campaigns/equipment/fellowship/FellowshipEquipmentCampaign.tsx
+++ web-react-ts/src/pages/campaigns/equipment/fellowship/FellowshipEquipmentCampaign.tsx
@@ -3,11 +3,15 @@ import { Container } from 'react-bootstrap'
 import MenuButton from '../../components/buttons/MenuButton'
 import { useNavigate } from 'react-router'
 import { MemberContext } from 'contexts/MemberContext'
-import { FELLOWSHIP_LATEST_EQUIPMENT_RECORD } from 'pages/campaigns/CampaignQueries'
+import {
+  FELLOWSHIP_LATEST_EQUIPMENT_RECORD,
+  EQUIPMENT_END_DATE,
+} from 'pages/campaigns/CampaignQueries'
 import { useQuery } from '@apollo/client'
 import { ChurchContext } from 'contexts/ChurchContext'
 import { HeadingPrimary } from 'components/HeadingPrimary/HeadingPrimary'
 import HeadingSecondary from 'components/HeadingSecondary'
+import { getHumanReadableDate } from 'jd-date-utils'
 
 const FellowshipEquipmentCampaign = () => {
   const { currentUser } = useContext(MemberContext)
@@ -16,6 +20,7 @@ const FellowshipEquipmentCampaign = () => {
   const church = currentUser.currentChurch
   const churchType = currentUser.currentChurch?.__typename
   const { fellowshipId } = useContext(ChurchContext)
+  const gatheringServiceId = currentUser?.gatheringService
 
   const { data } = useQuery(FELLOWSHIP_LATEST_EQUIPMENT_RECORD, {
     variables: {
@@ -25,6 +30,15 @@ const FellowshipEquipmentCampaign = () => {
 
   const fellowshipEquipmentRecord = data?.fellowships[0]?.equipmentRecord
 
+  const { data: equipmentEndDateData } = useQuery(EQUIPMENT_END_DATE, {
+    variables: {
+      gatheringServiceId: gatheringServiceId,
+    },
+  })
+
+  const equipmentEndDate =
+    equipmentEndDateData?.gatheringServices[0]?.equipmentEndDate
+
   return (
     <div className="d-flex align-items-center justify-content-center ">
       <Container>
@@ -32,6 +46,9 @@ const FellowshipEquipmentCampaign = () => {
           <HeadingPrimary>{`${church?.name} ${churchType}`}</HeadingPrimary>
           <HeadingSecondary>Equipment Campaign</HeadingSecondary>
         </div>
+        <h6 className="text-danger">
+          Current Deadline : {getHumanReadableDate(equipmentEndDate)}{' '}
+        </h6>
         <div className="d-grid gap-2 mt-4 text-center px-4">
           {fellowshipEquipmentRecord === null && (
             <MenuButton
--- web-react-ts/src/pages/campaigns/equipment/gathering-service/GatheringServiceEquipmentCampaign.tsx
+++ web-react-ts/src/pages/campaigns/equipment/gathering-service/GatheringServiceEquipmentCampaign.tsx
@@ -5,12 +5,24 @@ import { useNavigate } from 'react-router'
 import { MemberContext } from 'contexts/MemberContext'
 import { HeadingPrimary } from 'components/HeadingPrimary/HeadingPrimary'
 import HeadingSecondary from 'components/HeadingSecondary'
+import { EQUIPMENT_END_DATE } from 'pages/campaigns/CampaignQueries'
+import { useQuery } from '@apollo/client'
+import { getHumanReadableDate } from 'jd-date-utils'
 
 const GatheringServiceEquipmentCampaign = () => {
   const { currentUser } = useContext(MemberContext)
   const navigate = useNavigate()
 
   const church = currentUser.currentChurch
+  const gatheringServiceId = currentUser?.gatheringService
+
+  const { data } = useQuery(EQUIPMENT_END_DATE, {
+    variables: {
+      gatheringServiceId: gatheringServiceId,
+    },
+  })
+
+  const equipmentEndDate = data?.gatheringServices[0]?.equipmentEndDate
 
   return (
     <div className="d-flex align-items-center justify-content-center ">
@@ -19,6 +31,9 @@ const GatheringServiceEquipmentCampaign = () => {
           <HeadingPrimary>{`${church?.name} Gathering Service`}</HeadingPrimary>
           <HeadingSecondary>Equipment Campaign</HeadingSecondary>
         </div>
+        <h6 className="text-danger">
+          Current Deadline : {getHumanReadableDate(equipmentEndDate)}{' '}
+        </h6>
         <div className="d-grid gap-2 mt-4 text-center px-4">
           <MenuButton
             name="View Trends"
