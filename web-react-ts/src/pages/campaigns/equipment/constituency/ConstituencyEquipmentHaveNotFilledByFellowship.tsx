29179bbef9d08705a39e735b60a5f829d0ac9393
ahadziii <36607706+ahadziii@users.noreply.github.com>
2022-09-09 14:08:16 +0000

    feat: implement pull to refresh feature on equipment defaulters pages (#278)

--- web-react-ts/src/pages/campaigns/equipment/constituency/ConstituencyEquipmentHaveNotFilledByFellowship.tsx
+++ web-react-ts/src/pages/campaigns/equipment/constituency/ConstituencyEquipmentHaveNotFilledByFellowship.tsx
@@ -10,6 +10,7 @@ import { CONSTITUENCY_EQUIPMENT_DEFAULTERS_LIST_BY_FELLOWSHIP } from 'pages/camp
 import ApolloWrapper from 'components/base-component/ApolloWrapper'
 import { HeadingPrimary } from 'components/HeadingPrimary/HeadingPrimary'
 import HeadingSecondary from 'components/HeadingSecondary'
+import PullToRefresh from 'react-simple-pull-to-refresh'
 
 const ConstituencyEquipmentHaveNotFilledByFellowship = () => {
   const { currentUser } = useContext(MemberContext)
@@ -18,7 +19,7 @@ const ConstituencyEquipmentHaveNotFilledByFellowship = () => {
   const churchType = currentUser.currentChurch?.__typename
   const { constituencyId } = useContext(ChurchContext)
 
-  const { data, loading, error } = useQuery(
+  const { data, loading, error, refetch } = useQuery(
     CONSTITUENCY_EQUIPMENT_DEFAULTERS_LIST_BY_FELLOWSHIP,
     {
       variables: {
@@ -30,26 +31,30 @@ const ConstituencyEquipmentHaveNotFilledByFellowship = () => {
   const defaulters = data?.constituencies[0]?.fellowshipEquipmentNotFilled
 
   return (
-    <ApolloWrapper data={data} loading={loading} error={error}>
-      <div className="d-flex align-items-center justify-content-center ">
-        <Container>
-          <div className="text-center">
-            <HeadingPrimary>{`${church?.name} ${churchType}`}</HeadingPrimary>
-            <HeadingSecondary>Equipment Campaign</HeadingSecondary>
-          </div>
-          <h6 className="mt-4">Fellowships that haven't filled their form</h6>
+    <PullToRefresh
+      onRefresh={() => refetch({ constituencyId: constituencyId })}
+    >
+      <ApolloWrapper data={data} loading={loading} error={error}>
+        <div className="d-flex align-items-center justify-content-center ">
           <Container>
-            <Row>
-              {defaulters?.map(
-                (defaulter: EquipmentDefaulterProps, index: number) => (
-                  <DefaultersCard key={index} defaulter={defaulter} />
-                )
-              )}
-            </Row>
+            <div className="text-center">
+              <HeadingPrimary>{`${church?.name} ${churchType}`}</HeadingPrimary>
+              <HeadingSecondary>Equipment Campaign</HeadingSecondary>
+            </div>
+            <h6 className="mt-4">Fellowships that haven't filled their form</h6>
+            <Container>
+              <Row>
+                {defaulters?.map(
+                  (defaulter: EquipmentDefaulterProps, index: number) => (
+                    <DefaultersCard key={index} defaulter={defaulter} />
+                  )
+                )}
+              </Row>
+            </Container>
           </Container>
-        </Container>
-      </div>
-    </ApolloWrapper>
+        </div>
+      </ApolloWrapper>
+    </PullToRefresh>
   )
 }
 
--- web-react-ts/src/pages/campaigns/equipment/council/CouncilEquipmentHaveNotFilledByFellowship.tsx
+++ web-react-ts/src/pages/campaigns/equipment/council/CouncilEquipmentHaveNotFilledByFellowship.tsx
@@ -10,6 +10,7 @@ import { COUNCIL_EQUIPMENT_DEFAULTERS_LIST_BY_FELLOWSHIP } from 'pages/campaigns
 import ApolloWrapper from 'components/base-component/ApolloWrapper'
 import { HeadingPrimary } from 'components/HeadingPrimary/HeadingPrimary'
 import HeadingSecondary from 'components/HeadingSecondary'
+import PullToRefresh from 'react-simple-pull-to-refresh'
 
 const CouncilEquipmentHaveNotFilledByFellowship = () => {
   const { currentUser } = useContext(MemberContext)
@@ -18,7 +19,7 @@ const CouncilEquipmentHaveNotFilledByFellowship = () => {
   const churchType = currentUser.currentChurch?.__typename
   const { councilId } = useContext(ChurchContext)
 
-  const { data, loading, error } = useQuery(
+  const { data, loading, error, refetch } = useQuery(
     COUNCIL_EQUIPMENT_DEFAULTERS_LIST_BY_FELLOWSHIP,
     {
       variables: {
@@ -30,27 +31,29 @@ const CouncilEquipmentHaveNotFilledByFellowship = () => {
   const defaulters = data?.councils[0]?.fellowshipEquipmentNotFilled
 
   return (
-    <ApolloWrapper data={data} loading={loading} error={error}>
-      <div className="d-flex align-items-center justify-content-center ">
-        <Container>
-          <div className="text-center">
-            <HeadingPrimary>{`${church?.name} ${churchType}`}</HeadingPrimary>
-            <HeadingSecondary>Equipment Campaign</HeadingSecondary>
-          </div>
-          <h6 className="mt-4">Fellowships that haven't filled their form</h6>
-
+    <PullToRefresh onRefresh={() => refetch({ councilId: councilId })}>
+      <ApolloWrapper data={data} loading={loading} error={error}>
+        <div className="d-flex align-items-center justify-content-center ">
           <Container>
-            <Row>
-              {defaulters?.map(
-                (defaulter: EquipmentDefaulterProps, index: number) => (
-                  <DefaultersCard key={index} defaulter={defaulter} />
-                )
-              )}
-            </Row>
+            <div className="text-center">
+              <HeadingPrimary>{`${church?.name} ${churchType}`}</HeadingPrimary>
+              <HeadingSecondary>Equipment Campaign</HeadingSecondary>
+            </div>
+            <h6 className="mt-4">Fellowships that haven't filled their form</h6>
+
+            <Container>
+              <Row>
+                {defaulters?.map(
+                  (defaulter: EquipmentDefaulterProps, index: number) => (
+                    <DefaultersCard key={index} defaulter={defaulter} />
+                  )
+                )}
+              </Row>
+            </Container>
           </Container>
-        </Container>
-      </div>
-    </ApolloWrapper>
+        </div>
+      </ApolloWrapper>
+    </PullToRefresh>
   )
 }
 
