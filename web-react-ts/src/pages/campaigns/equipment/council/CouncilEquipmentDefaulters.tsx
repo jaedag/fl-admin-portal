ca0a87a7d9b7c7b08fa9235860aa4b34295eed4a
Edem Ahadzi <ahadzi.airdem@gmail.com>
2022-09-09 08:32:40 +0000

    feat: implement pull to refresh feature on equipment defaulters pages

--- web-react-ts/src/pages/campaigns/equipment/council/CouncilEquipmentDefaulters.tsx
+++ web-react-ts/src/pages/campaigns/equipment/council/CouncilEquipmentDefaulters.tsx
@@ -9,6 +9,7 @@ import { COUNCIL_EQUIPMENT_DEFAULTERS_NUMBER_BY_CONSTITUENCY_AND_FELLOWSHIP } fr
 import ApolloWrapper from 'components/base-component/ApolloWrapper'
 import { HeadingPrimary } from 'components/HeadingPrimary/HeadingPrimary'
 import HeadingSecondary from 'components/HeadingSecondary'
+import PullToRefresh from 'react-simple-pull-to-refresh'
 
 const CouncilEquipmentDefaulters = () => {
   const { currentUser } = useContext(MemberContext)
@@ -18,7 +19,7 @@ const CouncilEquipmentDefaulters = () => {
   const churchType = currentUser.currentChurch?.__typename
   const { councilId } = useContext(ChurchContext)
 
-  const { data, loading, error } = useQuery(
+  const { data, loading, error, refetch } = useQuery(
     COUNCIL_EQUIPMENT_DEFAULTERS_NUMBER_BY_CONSTITUENCY_AND_FELLOWSHIP,
     {
       variables: {
@@ -29,75 +30,79 @@ const CouncilEquipmentDefaulters = () => {
   const council = data?.councils[0]
 
   return (
-    <ApolloWrapper data={data} loading={loading} error={error}>
-      <div className="d-flex align-items-center justify-content-center ">
-        <Container>
-          <div className="text-center">
-            <HeadingPrimary>{`${church?.name} ${churchType}`}</HeadingPrimary>
-            <HeadingSecondary>Equipment Campaign</HeadingSecondary>
-          </div>
-          <h6 className="mt-4">
-            Fellowships and Constituencies that haven't filled their form
-          </h6>
-          <div className=" gap-2 mt-4">
-            <h6>
-              Fellowships :{' '}
-              <span className="text-primary">{council?.fellowshipCount}</span>
+    <PullToRefresh onRefresh={() => refetch({ councilId: councilId })}>
+      <ApolloWrapper data={data} loading={loading} error={error}>
+        <div className="d-flex align-items-center justify-content-center ">
+          <Container>
+            <div className="text-center">
+              <HeadingPrimary>{`${church?.name} ${churchType}`}</HeadingPrimary>
+              <HeadingSecondary>Equipment Campaign</HeadingSecondary>
+            </div>
+            <h6 className="mt-4">
+              Fellowships and Constituencies that haven't filled their form
             </h6>
-            <Row className="mt-3">
-              <Col>
-                <DefaultersMenuButton
-                  name="Have not filled"
-                  onClick={() => {
-                    navigate(
-                      `/campaigns/council/equipment/have-not-filled/fellowship`
-                    )
-                  }}
-                  number={council?.fellowshipEquipmentNotFilledCount}
-                  color="text-danger"
-                />
-              </Col>
-              <Col>
-                <DefaultersMenuButton
-                  name="Filled"
-                  onClick={() => {}}
-                  number={council?.fellowshipEquipmentFilledCount}
-                  color="text-success"
-                />
-              </Col>
-            </Row>
-          </div>
-          <div className=" gap-2 mt-4">
-            <h6>
-              Constituencies :{' '}
-              <span className="text-primary">{council?.constituencyCount}</span>
-            </h6>
-            <Row className="mt-3">
-              <Col>
-                <DefaultersMenuButton
-                  name="Have not filled"
-                  onClick={() => {
-                    navigate(
-                      `/campaigns/council/equipment/have-not-filled/constituency`
-                    )
-                  }}
-                  number={council?.constituencyEquipmentNotFilledCount}
-                  color="text-danger"
-                />
-              </Col>
-              <Col>
-                <DefaultersMenuButton
-                  name="Filled"
-                  onClick={() => {}}
-                  number={council?.constituencyEquipmentFilledCount}
-                  color="text-success"
-                />
-              </Col>
-            </Row>
-          </div>
-        </Container>
-      </div>
-    </ApolloWrapper>
+            <div className=" gap-2 mt-4">
+              <h6>
+                Fellowships :{' '}
+                <span className="text-primary">{council?.fellowshipCount}</span>
+              </h6>
+              <Row className="mt-3">
+                <Col>
+                  <DefaultersMenuButton
+                    name="Have not filled"
+                    onClick={() => {
+                      navigate(
+                        `/campaigns/council/equipment/have-not-filled/fellowship`
+                      )
+                    }}
+                    number={council?.fellowshipEquipmentNotFilledCount}
+                    color="text-danger"
+                  />
+                </Col>
+                <Col>
+                  <DefaultersMenuButton
+                    name="Filled"
+                    onClick={() => {}}
+                    number={council?.fellowshipEquipmentFilledCount}
+                    color="text-success"
+                  />
+                </Col>
+              </Row>
+            </div>
+            <div className=" gap-2 mt-4">
+              <h6>
+                Constituencies :{' '}
+                <span className="text-primary">
+                  {council?.constituencyCount}
+                </span>
+              </h6>
+              <Row className="mt-3">
+                <Col>
+                  <DefaultersMenuButton
+                    name="Have not filled"
+                    onClick={() => {
+                      navigate(
+                        `/campaigns/council/equipment/have-not-filled/constituency`
+                      )
+                    }}
+                    number={council?.constituencyEquipmentNotFilledCount}
+                    color="text-danger"
+                  />
+                </Col>
+                <Col>
+                  <DefaultersMenuButton
+                    name="Filled"
+                    onClick={() => {}}
+                    number={council?.constituencyEquipmentFilledCount}
+                    color="text-success"
+                  />
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </div>
+      </ApolloWrapper>
+    </PullToRefresh>
   )
 }
 
--- web-react-ts/src/pages/campaigns/equipment/council/CouncilEquipmentHaveNotFilledByConstituency.tsx
+++ web-react-ts/src/pages/campaigns/equipment/council/CouncilEquipmentHaveNotFilledByConstituency.tsx
@@ -10,6 +10,7 @@ import { COUNCIL_EQUIPMENT_DEFAULTERS_LIST_BY_CONSTITUENCY } from 'pages/campaig
 import ApolloWrapper from 'components/base-component/ApolloWrapper'
 import { HeadingPrimary } from 'components/HeadingPrimary/HeadingPrimary'
 import HeadingSecondary from 'components/HeadingSecondary'
+import PullToRefresh from 'react-simple-pull-to-refresh'
 
 function CouncilEquipmentHaveNotFilledByConstituency() {
   const { currentUser } = useContext(MemberContext)
@@ -18,7 +19,7 @@ function CouncilEquipmentHaveNotFilledByConstituency() {
   const churchType = currentUser.currentChurch?.__typename
   const { councilId } = useContext(ChurchContext)
 
-  const { data, loading, error } = useQuery(
+  const { data, loading, error, refetch } = useQuery(
     COUNCIL_EQUIPMENT_DEFAULTERS_LIST_BY_CONSTITUENCY,
     {
       variables: {
@@ -30,29 +31,31 @@ function CouncilEquipmentHaveNotFilledByConstituency() {
   const defaulters = data?.councils[0]?.constituencyEquipmentNotFilled
 
   return (
-    <ApolloWrapper data={data} loading={loading} error={error}>
-      <div className="d-flex align-items-center justify-content-center ">
-        <Container>
-          <div className="text-center">
-            <HeadingPrimary>{`${church?.name} ${churchType}`}</HeadingPrimary>
-            <HeadingSecondary>Equipment Campaign</HeadingSecondary>
-          </div>
-          <h6 className="mt-4">
-            Constituencies that haven't filled their form
-          </h6>
-
+    <PullToRefresh onRefresh={() => refetch({ councilId: councilId })}>
+      <ApolloWrapper data={data} loading={loading} error={error}>
+        <div className="d-flex align-items-center justify-content-center ">
           <Container>
-            <Row>
-              {defaulters?.map(
-                (defaulter: EquipmentDefaulterProps, index: number) => (
-                  <DefaultersCard key={index} defaulter={defaulter} />
-                )
-              )}
-            </Row>
+            <div className="text-center">
+              <HeadingPrimary>{`${church?.name} ${churchType}`}</HeadingPrimary>
+              <HeadingSecondary>Equipment Campaign</HeadingSecondary>
+            </div>
+            <h6 className="mt-4">
+              Constituencies that haven't filled their form
+            </h6>
+
+            <Container>
+              <Row>
+                {defaulters?.map(
+                  (defaulter: EquipmentDefaulterProps, index: number) => (
+                    <DefaultersCard key={index} defaulter={defaulter} />
+                  )
+                )}
+              </Row>
+            </Container>
           </Container>
-        </Container>
-      </div>
-    </ApolloWrapper>
+        </div>
+      </ApolloWrapper>
+    </PullToRefresh>
   )
 }
 
