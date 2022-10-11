d1efef62dcd86024ef0ac648f40ed86d796acb1b
John-Dag Addy <jaedagy@gmail.com>
2022-10-11 11:17:41 +0000

    fix(paystack): remove math.round around banking amount

--- api/src/resolvers/banking/banking-resolver.ts
+++ api/src/resolvers/banking/banking-resolver.ts
@@ -127,7 +127,7 @@ const bankingMutation = {
         Authorization: auth,
       },
       data: {
-        amount: Math.round(serviceRecord.income * (100 / 98.05) * 100),
+        amount: serviceRecord.income * (100 / 98.05) * 100,
         email: cypherResponse.author.email,
         currency: 'GHS',
         mobile_money: {
