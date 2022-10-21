9849978932df99f53f2302dc76fc4e175e5c7e12
John-Dag Addy <jaedagy@gmail.com>
2022-10-21 21:41:33 +0000

    fix: fix ux bugs for paystack edge cases

--- api/src/resolvers/banking/banking-cypher.ts
+++ api/src/resolvers/banking/banking-cypher.ts
@@ -68,7 +68,7 @@ RETURN record {
 
 export const setTransactionStatusFailed = `
 MATCH (record:ServiceRecord {id: $serviceRecordId})
-MATCH (record)-[r:OFFERING_BANKED_BY]->(banker)
+OPTIONAL MATCH (record)-[r:OFFERING_BANKED_BY]->(banker)
 SET record.transactionStatus = 'failed'
 
 DELETE r
