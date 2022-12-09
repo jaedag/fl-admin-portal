dd898e7e41cc151628ff80ec9866c25c17437629
John-Dag Addy <jaedagy@gmail.com>
2022-12-09 14:29:39 +0000

    fix: fix breaking change in incomeCharges data type

--- web-react-ts/src/pages/services/banking/self-banking/PayOffering.tsx
+++ web-react-ts/src/pages/services/banking/self-banking/PayOffering.tsx
@@ -51,9 +51,8 @@ const PayOffering = (props: PayOfferingProps) => {
   const [ConfirmOfferingPayment] = useMutation(CONFIRM_OFFERING_PAYMENT)
   const navigate = useNavigate()
   const service = data?.serviceRecords[0]
-  const incomeAndCharges = parseFloat(
-    (service?.income / (1 - 0.0195) + 0.01).toFixed(2)
-  ).toFixed(2)
+  const incomeAndCharges = (service?.income / (1 - 0.0195) + 0.01).toFixed(2)
+
   const { togglePopup, isOpen } = usePopup()
   const { show, handleClose, handleShow } = useModal()
   const [errorMessage, setErrorMessage] = useState('')
@@ -232,7 +231,7 @@ const PayOffering = (props: PayOfferingProps) => {
                       <Col>
                         <small className="form-text label ">Charges</small>
                         <div className="fw-bold yellow">
-                          {incomeAndCharges - service?.income} GHS
+                          {parseFloat(incomeAndCharges) - service?.income} GHS
                         </div>
                       </Col>
                     </Row>
