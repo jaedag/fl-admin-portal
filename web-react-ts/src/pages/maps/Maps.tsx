d8990ebfb45587268acf9a5c8caf221a40b7997a
John-Dag Addy <jaedagy@gmail.com>
2023-03-14 14:32:41 +0000

    fix: redesign of maps button

--- web-react-ts/src/pages/maps/Maps.tsx
+++ web-react-ts/src/pages/maps/Maps.tsx
@@ -1,9 +1,8 @@
 import { HeadingPrimary } from 'components/HeadingPrimary/HeadingPrimary'
-import { Container } from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
 import HeadingSecondary from 'components/HeadingSecondary'
-import MenuButton from 'components/buttons/MenuButton'
-import { Map } from 'react-bootstrap-icons'
 import { useNavigate } from 'react-router'
+import { BiMap } from 'react-icons/bi'
 
 const Maps = () => {
   const navigate = useNavigate()
@@ -14,17 +13,16 @@ const Maps = () => {
       <HeadingSecondary>Click on one of churches below</HeadingSecondary>
       <div className="py-3" />
       <div className="d-grid gap-2 text-left">
-        <MenuButton
-          title="View Map"
-          iconComponent={Map}
-          iconBg={true}
-          noCaption
-          iconCaption={`Map`}
+        <Button
+          size="lg"
+          className="text-start py-4"
+          variant="dark"
           onClick={() => {
             navigate(`/maps/view-maps`)
           }}
-          color={`maps`}
-        />
+        >
+          <BiMap /> View Map
+        </Button>
       </div>
     </Container>
   )
