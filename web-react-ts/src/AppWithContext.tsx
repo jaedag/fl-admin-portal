6d04bb313de03b81e0c10f352ba2d3d8db8b3950
John-Dag Addy <jaedagy@gmail.com>
2023-03-14 10:49:49 +0000

    chore: update package dependencies and update sentry/react

--- web-react-ts/src/AppWithContext.tsx
+++ web-react-ts/src/AppWithContext.tsx
@@ -125,7 +125,14 @@ const AppWithContext = (props: AppPropsType) => {
 
   Sentry.init({
     dsn: 'https://a6fccd390f7a4cdfa48da901b0e2e22f@o1423098.ingest.sentry.io/6770463',
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
+    // If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
     integrations: [
+      new Sentry.Replay(),
       new BrowserTracing({
         routingInstrumentation: Sentry.reactRouterV6Instrumentation(
           React.useEffect,
