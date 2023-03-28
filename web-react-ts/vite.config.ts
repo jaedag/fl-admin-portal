da1811deffbca269661eee909e62380ba6892be0
John-Dag Addy <jaedagy@gmail.com>
2023-03-28 13:42:28 +0000

    fix: update vite config to start server at localhost instead of 127.0.0.1

--- web-react-ts/vite.config.ts
+++ web-react-ts/vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
+import dns from 'dns'
 
 // https://vitejs.dev/config/
+
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
   server: {
     open: true,
