99be4477ff33356ec586f0532cdd6099776fb081
John-Dag Addy <jaedagy@gmail.com>
2023-04-21 14:01:12 +0000

    feat: add howYouJoined property on user profile page
    add edit page button

--- web-react-ts/src/pages/directory/user-profile/EditPage.tsx
+++ web-react-ts/src/pages/directory/user-profile/EditPage.tsx
@@ -44,9 +44,10 @@ const UserProfileEditPage = () => {
     maritalStatus: member?.maritalStatus ? member?.maritalStatus.status : '',
     occupation: member?.occupation ? member?.occupation.occupation : '',
     pictureUrl: member?.pictureUrl ? member?.pictureUrl : '',
-    fellowship: memberChurch?.fellowship?.name ?? '',
+    fellowship: memberChurch?.fellowship ?? '',
     idlLocation: member?.idlLocation ?? '',
     ministry: memberChurch?.ministry ? memberChurch?.ministry.id : '',
+    howYouJoined: member?.howYouJoined ? member?.howYouJoined : '',
   }
 
   const [UpdateMember] = useMutation(UPDATE_MEMBER_MUTATION)
@@ -72,8 +73,9 @@ const UserProfileEditPage = () => {
         maritalStatus: values.maritalStatus,
         occupation: values.occupation,
         pictureUrl: values.pictureUrl,
+        howYouJoined: values.howYouJoined,
 
-        fellowship: values.fellowship,
+        fellowship: values.fellowship.id,
         ministry: values.ministry,
       },
     })
