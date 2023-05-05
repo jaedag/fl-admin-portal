ae86736b7c580fbc16324a57a4bf04bc24e4cefa
John-Dag Addy <jaedagy@gmail.com>
2023-05-05 04:45:17 +0000

    fix: add GMD as a currency option

--- web-react-ts/src/global-utils.ts
+++ web-react-ts/src/global-utils.ts
@@ -63,6 +63,7 @@ export const STREAM_SERVICE_DAY_OPTIONS: FormikSelectOptions = [
 export const CURRENCY_OPTIONS: FormikSelectOptions = [
   { key: 'GHS', value: 'GHS' },
   { key: 'MGA', value: 'MGA' },
+  { key: 'GMD', value: 'GMD' },
   { key: 'ZMW', value: 'ZMW' },
 ]
 
