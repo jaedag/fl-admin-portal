fc6afdd9e60b52b8c787aac619a1254cabd97045
John-Dag Addy <jaedagy@gmail.com>
2023-05-18 23:07:40 +0000

    fix: update member deletion options

--- web-react-ts/src/global-utils.ts
+++ web-react-ts/src/global-utils.ts
@@ -71,7 +71,7 @@ export const CURRENCY_OPTIONS: FormikSelectOptions = [
 ]
 
 export const DELETE_MEMBER_CATEGORY_OPTIONS: FormikSelectOptions = [
-  { key: 'Moved Out of Accra', value: 'Moved Out of Accra' },
+  { key: 'Moved Cities', value: 'Moved Cities' },
   {
     key: 'Left the Church In Spite of Calls and Visits',
     value: 'Left the Church In Spite of Calls and Visits',
