d39d21962b864bc83823eb09d4bcf7427bb2cd45
John-Dag Addy <jaedagy@gmail.com>
2023-05-26 20:16:58 +0000

    fix: update with encrypt-secrets

---
+++ scripts/encrypt-secrets.js
@@ -0,0 +1,9 @@
+// ./bin/encrypt-secrets.js
+const secrets = require('gitops-secrets')
+
+async function main() {
+  const payload = await secrets.providers.doppler.fetch()
+  secrets.build(payload, { path: 'lib/secrets.js' })
+}
+
+main()
