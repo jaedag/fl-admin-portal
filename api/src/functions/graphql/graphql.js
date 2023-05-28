14ffd1c8f790e1e4692ebaace37daaad343c3bad
John-Dag Addy <jaedagy@gmail.com>
2023-05-28 15:28:27 +0000

    fix: log out secrets 2

--- api/src/functions/graphql/graphql.js
+++ api/src/functions/graphql/graphql.js
@@ -13,7 +13,7 @@ const { typeDefs } = require('./schema/graphql-schema')
 const resolvers = require('../../resolvers/resolvers').default
 const SECRETS = require('../../resolvers/getSecrets').default
 
-console.log(SECRETS)
+console.log('SECRETS', SECRETS)
 Sentry.init({
   dsn: 'https://cd02d9dbb24041f88bfa297993779123@o1423098.ingest.sentry.io/6770464',
 
