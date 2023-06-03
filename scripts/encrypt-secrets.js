ebd95d715dcaecacdd900b9d60eba6b27aa75210
John-Dag Addy <jaedagy@gmail.com>
2023-06-03 15:42:05 +0000

    fix: change secrets to secrets.ts

--- scripts/encrypt-secrets.js
+++ scripts/encrypt-secrets.js
@@ -4,7 +4,7 @@ const secrets = require('gitops-secrets')
 async function main() {
   const payload = await secrets.providers.doppler.fetch()
   secrets.build(payload, { path: 'lib/secrets.js' })
-  secrets.build(payload, { path: 'api/src/resolvers/secrets.js' })
+  secrets.build(payload, { path: 'api/src/resolvers/secrets.ts' })
   secrets.build(payload, { path: 'api/src/functions/payment/secrets.js' })
   secrets.build(payload, {
     path: 'api/src/functions/code-of-the-day/secrets.js',
