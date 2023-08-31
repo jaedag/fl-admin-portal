0257b500af752f45a1f6e5b93745e4b39bb93673
John-Dag Addy <jaedagy@gmail.com>
2023-08-31 22:32:11 +0000

    fix: fix search page on mobile and desktop

--- web-react-ts/src/components/SearchBox.tsx
+++ web-react-ts/src/components/SearchBox.tsx
@@ -1,35 +1,62 @@
 import { useContext } from 'react'
-import { Button, InputGroup, Form, Nav } from 'react-bootstrap'
+import { Button, InputGroup, Form as bsForm, Nav } from 'react-bootstrap'
 import { SearchContext } from 'contexts/MemberContext'
 import './SearchBox.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { Form, Formik, FormikHelpers } from 'formik'
+import * as Yup from 'yup'
 
 const SearchBox = () => {
   const { setSearchKey } = useContext(SearchContext)
+  const navigate = useNavigate()
+  const initialValues = {
+    searchKeyVal: '',
+  }
+  const validationSchema = Yup.object({
+    searchKeyVal: Yup.string().required(''),
+  })
+
+  const onSubmit = (
+    values: typeof initialValues,
+    onSubmitProps: FormikHelpers<typeof initialValues>
+  ) => {
+    onSubmitProps.setSubmitting(true)
+    setSearchKey(values.searchKeyVal)
+    navigate('/search-results')
+    onSubmitProps.setSubmitting(false)
+  }
 
   return (
-    <Form onSubmit={(e) => e.preventDefault()}>
-      <InputGroup className="mt-4">
-        <Form.Control
-          name="searchKeyVal"
-          className="nav-search-box"
-          placeholder="Search for anything..."
-          aria-label="Search for anything..."
-          aria-describedby="submit-search"
-          onChange={(e) => setSearchKey(e.target.value)}
-        />
-        <Button id="submit-search" variant="success" type="submit">
-          <Nav.Link
-            as={Link}
-            eventKey={10}
-            to="/search-results"
-            className="p-0"
-          >
-            Search
-          </Nav.Link>
-        </Button>
-      </InputGroup>
-    </Form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {() => (
+        <Form>
+          <InputGroup className="mt-4">
+            <bsForm.Control
+              name="searchKeyVal"
+              className="nav-search-box"
+              placeholder="Search for anything..."
+              aria-label="Search for anything..."
+              aria-describedby="submit-search"
+              onChange={(e) => setSearchKey(e.target.value)}
+            />
+            <Button id="submit-search" variant="success" type="submit">
+              <Nav.Link
+                as={Link}
+                eventKey={10}
+                to="/search-results"
+                className="p-0"
+              >
+                Search
+              </Nav.Link>
+            </Button>
+          </InputGroup>
+        </Form>
+      )}
+    </Formik>
   )
 }
 
