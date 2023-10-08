a497d390f87c659b83c719fa7bc60f5dc143283c
John-Dag Addy <jaedagy@gmail.com>
2023-10-08 20:05:40 +0000

    fix: permission for stream admins  to create bacentas

--- web-react-ts/src/pages/directory/display/AllBacentas.tsx
+++ web-react-ts/src/pages/directory/display/AllBacentas.tsx
@@ -58,7 +58,7 @@ const DisplayAllBacentas = () => {
               </Link>
             ) : null}
           </Col>
-          <RoleView roles={permitAdminArrivals('Campus')}>
+          <RoleView roles={permitAdminArrivals('Stream')}>
             <Col className="col-auto">
               <Link
                 to="/bacenta/addbacenta"
