0fe7243fbf47f4903295907df61663ad8e56fc0f
Nathaniel Asiak <nathaniel.asiak@acity.edu.gh>
2023-10-19 14:57:22 +0000

    fix: resolver for closing overight, streams, campuses and councils

--- api/src/resolvers/cypher/close-church-cypher.ts
+++ api/src/resolvers/cypher/close-church-cypher.ts
@@ -5,7 +5,7 @@ RETURN fellowship.name AS name, COUNT(member) AS memberCount
 `
 
 export const getLastServiceRecord = `
-MATCH (church {id: $churchId}) WHERE church:Fellowship OR church:Constituency OR church:Council
+MATCH (church {id: $churchId}) WHERE church:Fellowship OR church:Constituency OR church:Council OR church:Stream OR church:Campus
 MATCH (church)-[:HAS_HISTORY]->(:ServiceLog)-[:HAS_SERVICE]->(otherRecords:ServiceRecord)-[:SERVICE_HELD_ON]->(otherDate:TimeGraph)
 WHERE NOT (otherRecords:NoService) AND duration.between(otherDate.date, date()).weeks < 52
 
@@ -25,6 +25,27 @@ export const checkConstituencyHasNoMembers = `
 MATCH (constituency:Constituency {id:$constituencyId})
 MATCH (constituency)-[:HAS]->(bacentas:Bacenta)<-[:LEADS]-(member:Active:Member)
 RETURN constituency.name AS name, COUNT(member) AS memberCount, COUNT(bacentas) AS bacentaCount
+`
+export const checkCouncilHasNoMembers = `
+MATCH (council:Council {id:$councilId})
+MATCH (council)-[:HAS]->(constituencies:Constituency)<-[:LEADS]-(member:Active:Member)
+RETURN council.name AS name, COUNT(member) AS memberCount, COUNT(constituencies) AS constituencyCount
+`
+
+export const checkStreamHasNoMembers = `
+MATCH (stream:Stream {id:$streamId})
+MATCH (stream)-[:HAS]->(councils:Council)<-[:LEADS]-(member:Active:Member)
+RETURN stream.name AS name, COUNT(member) AS memberCount, COUNT(councils) AS councilCount
+`
+export const checkCampusHasNoMembers = `
+MATCH (campus:Campus {id:$campusId})
+MATCH (campus)-[:HAS]->(streams:Stream)<-[:LEADS]-(member:Active:Member)
+RETURN campus.name AS name, COUNT(member) AS memberCount, COUNT(streams) AS streamCount
+`
+export const checkOversightHasNoMembers = `
+MATCH (oversight:Oversight {id:$oversightId})
+MATCH (oversight)-[:HAS]->(campuses:Campus)<-[:LEADS]-(member:Active:Member)
+RETURN oversight.name AS name, COUNT(member) AS memberCount, COUNT(campuses) AS campusCount
 `
 
 export const closeDownFellowship = `
@@ -111,5 +132,111 @@ REMOVE constituency:Constituency,bacentas:Bacenta,fellowships:Fellowship
 RETURN council {
   .id, .name,
   constituencies: [constituencies {.id, .name}]
+}
+`
+
+export const closeDownCouncil = `
+MATCH (council:Council {id:$councilId})<-[:HAS]-(stream:Stream)
+WITH council, stream
+
+CREATE (log:HistoryLog {id:apoc.create.uuid()})
+  SET log.timeStamp = datetime(),
+  log.historyRecord = council.name + ' Council was closed down under ' + stream.name +' stream'
+
+WITH council, stream, log
+MATCH (admin:Member {auth_id: $auth.jwt.sub})
+MATCH (stream)-[:HAS]->(councils)
+OPTIONAL MATCH (council)-[:HAS]->(constituencies)-[:HAS]->(bacentas)-[:HAS]->(fellowships)
+
+MERGE (date:TimeGraph {date:date()})
+MERGE (log)-[:LOGGED_BY]->(admin)
+MERGE (log)-[:RECORDED_ON]->(date)
+MERGE (stream)-[:HAS_HISTORY]->(log)
+
+SET council:ClosedCouncil, constituencies:ClosedConstituency, bacentas:ClosedBacenta, fellowships:ClosedFellowship
+REMOVE council:Council, constituencies:Constituency,bacentas:Bacenta,fellowships:Fellowship
+
+RETURN stream {
+  .id, .name,
+  councils: [councils {.id, .name}]
+}
+`
+export const closeDownStream = `
+MATCH (stream:Stream {id:$streamId})<-[:HAS]-(campus:Campus)
+WITH stream, campus
+
+CREATE (log:HistoryLog {id:apoc.create.uuid()})
+  SET log.timeStamp = datetime(),
+  log.historyRecord = stream.name + ' Stream was closed down under ' + campus.name +' Campus'
+
+WITH stream, campus, log
+MATCH (admin:Member {auth_id: $auth.jwt.sub})
+MATCH (campus)-[:HAS]->(streams)
+OPTIONAL MATCH (stream)-[:HAS]->(councils)-[:HAS]->(constituencies)-[:HAS]->(bacentas)-[:HAS]->(fellowships)
+
+MERGE (date:TimeGraph {date:date()})
+MERGE (log)-[:LOGGED_BY]->(admin)
+MERGE (log)-[:RECORDED_ON]->(date)
+MERGE (campus)-[:HAS_HISTORY]->(log)
+
+SET  stream:ClosedStream, councils:ClosedCouncil, constituencies:ClosedConstituency, bacentas:ClosedBacenta, fellowships:ClosedFellowship
+REMOVE  stream:Stream, councils:Council, constituencies:Constituency, bacentas:Bacenta,fellowships:Fellowship
+
+RETURN campus {
+  .id, .name,
+  streams: [streams {.id, .name}]
+}
+`
+export const closeDownCampus = `
+MATCH (campus:Campus {id:$campusId})<-[:HAS]-(oversight:Oversight)
+WITH campus, oversight
+
+CREATE (log:HistoryLog {id:apoc.create.uuid()})
+  SET log.timeStamp = datetime(),
+  log.historyRecord = campus.name + ' Campus was closed down under ' + oversight.name +' Oversight'
+
+WITH campus, oversight, log
+MATCH (admin:Member {auth_id: $auth.jwt.sub})
+MATCH (oversight)-[:HAS]->(campuses)
+OPTIONAL MATCH (campus)-[:HAS]->(streams)-[:HAS]->(councils)-[:HAS]->(constituencies)-[:HAS]->(bacentas)-[:HAS]->(fellowships)
+
+MERGE (date:TimeGraph {date:date()})
+MERGE (log)-[:LOGGED_BY]->(admin)
+MERGE (log)-[:RECORDED_ON]->(date)
+MERGE (oversight)-[:HAS_HISTORY]->(log)
+
+SET  campus:ClosedCampus streams:ClosedStream, councils:ClosedCouncil, constituencies:ClosedConstituency, bacentas:ClosedBacenta, fellowships:ClosedFellowship
+REMOVE campus:Campus, streams:Stream, councils:Council, constituencies:Constituency, bacentas:Bacenta,fellowships:Fellowship
+
+RETURN oversight {
+  .id, .name,
+  campuses: [campuses {.id, .name}]
+}
+`
+
+export const closeDownOversight = `
+MATCH (oversight:OverSight {id:$oversightId})<-[:HAS]-(denomination:Denomination)
+WITH oversight, denomination
+
+CREATE (log:HistoryLog {id:apoc.create.uuid()})
+  SET log.timeStamp = datetime(),
+  log.historyRecord = oversight.name + ' Oversight was closed down under ' + denomination.name +' Denomination'
+
+WITH oversight, denomination, log
+MATCH (admin:Member {auth_id: $auth.jwt.sub})
+MATCH (denomination)-[:HAS]->(oversights)
+OPTIONAL MATCH (oversight)-[:HAS]->(campuses)-[:HAS]->(streams)-[:HAS]->(councils)-[:HAS]->(constituencies)-[:HAS]->(bacentas)-[:HAS]->(fellowships)
+
+MERGE (date:TimeGraph {date:date()})
+MERGE (log)-[:LOGGED_BY]->(admin)
+MERGE (log)-[:RECORDED_ON]->(date)
+MERGE (denomination)-[:HAS_HISTORY]->(log)
+
+SET  oversight:ClosedOversight, campuses:ClosedCampus streams:ClosedStream, councils:ClosedCouncil, constituencies:ClosedConstituency, bacentas:ClosedBacenta, fellowships:ClosedFellowship
+REMOVE oversight:Oversight, campus:Campus, streams:Stream, councils:Council, constituencies:Constituency, bacentas:Bacenta,fellowships:Fellowship
+
+RETURN denomination {
+  .id, .name,
+  oversights: [oversights {.id, .name}]
 }
 `
