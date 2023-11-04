1334ffc2ddd9b9dcb462cfb5d80584f99c6d80c3
Nathaniel Asiak <nathaniel.asiak@acity.edu.gh>
2023-11-04 12:05:10 +0000

    feat: on stage attendance

--- api/src/resolvers/services/rehearsal-cypher.ts
+++ api/src/resolvers/services/rehearsal-cypher.ts
@@ -8,6 +8,27 @@ WHERE date(date.date).week = date().week AND date(date.date).year = date().year 
 RETURN church.id AS id, church.name AS name, labels(church) AS labels, labels(higherChurch) AS higherChurchLabels, higherChurch.id AS higherChurchId, record IS NOT NULL AS alreadyFilled
 `
 
+export const checkStreamServiceDay = `
+WITH datetime($serviceDate) as dt
+MATCH (church {id: $churchId}) WHERE church:Ministry
+OPTIONAL MATCH (church)<-[:HAS_MINISTRY]-(stream:Stream)-[:MEETS_ON]->(serviceDay:ServiceDay {dayNumber: dt.dayOfWeek})
+RETURN serviceDay.day IS NOT NULL AS serviceDay
+`
+export const checkCurrentServiceLog = `
+MATCH (church {id:$churchId}) 
+WHERE church:Hub OR church:HubCouncil OR church:Ministry
+MATCH (church)-[:CURRENT_HISTORY]->(log:ServiceLog)
+RETURN true AS exists
+`
+export const getServantAndChurch = `
+MATCH (church {id: $churchId}) 
+WHERE church:Hub OR church:HubCouncil OR church:Ministry
+MATCH (church)<-[:LEADS]-(servant:Active:Member)
+UNWIND labels(church) AS churchType 
+WITH churchType, church, servant WHERE churchType IN ['Hub', 'HubCouncil','Ministry']
+RETURN church.id AS churchId, church.name AS churchName, servant.id AS servantId, servant.auth_id AS auth_id, servant.firstName AS firstName, servant.lastName AS lastName, churchType AS churchType
+`
+
 export const checkMinistryAttendanceFormFilledThisWeek = `
     MATCH (church {id: $churchId})
     WHERE church:HubFellowship OR church:Hub OR church:Ministry 
@@ -17,6 +38,16 @@ export const checkMinistryAttendanceFormFilledThisWeek = `
     WHERE date(date.date).week = date().week AND date(date.date).year = date().year AND (record:MinistryAttendanceRecord)
     RETURN church.id AS id, church.name AS name, labels(church) AS labels, labels(higherChurch) AS higherChurchLabels, higherChurch.id AS higherChurchId, record IS NOT NULL AS alreadyFilled
     `
+
+export const checkMinistryStageAttendanceFormFilledThisWeek = `
+MATCH (church {id: $churchId})
+WHERE church:Ministry 
+MATCH (church)<-[:HAS]-(higherChurch) WHERE higherChurch:CreativeArts
+
+OPTIONAL MATCH (church)-[:HAS_HISTORY]->(:ServiceLog)-[:HAS_SERVICE]->(record)-[:SERVICE_HELD_ON]->(date)    
+WHERE date(date.date).week = date().week AND date(date.date).year = date().year AND (record:MinistryStageAttendanceRecord)
+RETURN church.id AS id, church.name AS name, labels(church) AS labels, labels(higherChurch) AS higherChurchLabels, higherChurch.id AS higherChurchId, record IS NOT NULL AS alreadyFilled
+`
 
 export const recordSundayMinistryAttendance = `
     CREATE (ministryAttendanceRecord:MinistryAttendanceRecord {id: apoc.create.uuid()})
@@ -89,6 +120,35 @@ MATCH (treasurer:Active:Member {id: treasurerId})
 MERGE (treasurer)-[:WAS_TREASURER_FOR]->(rehearsalRecord)
 
 RETURN rehearsalRecord
+`
+
+export const recordOnStageAttendance = `
+    CREATE (ministryStageAttendanceRecord:MinistryStageAttendanceRecord {id: apoc.create.uuid()})
+        SET ministryStageAttendanceRecord.createdAt = datetime(), 
+        ministryStageAttendanceRecord.attendance = $attendance, 
+        ministryStageAttendanceRecord.onStagePictures = $onStagePictures
+    
+    WITH ministryStageAttendanceRecord
+    MATCH (church {id: $churchId}) WHERE church:Ministry
+    MATCH (church)-[current:CURRENT_HISTORY]->(log:ServiceLog)
+    MATCH (leader:Member {auth_id: $auth.jwt.sub})
+
+    MERGE (serviceDate:TimeGraph {date: date($serviceDate)})
+
+    WITH DISTINCT ministryStageAttendanceRecord, leader, serviceDate, log 
+    MERGE (ministryStageAttendanceRecord)-[:LOGGED_BY]->(leader)
+    MERGE (ministryStageAttendanceRecord)-[:SERVICE_HELD_ON]->(serviceDate)
+    MERGE (log)-[:HAS_SERVICE]->(ministryStageAttendanceRecord)
+
+    WITH log, ministryStageAttendanceRecord
+    MERGE (aggregate:AggregateRehearsalRecord {id: date().week + '-' + date().year + '-' + log.id, week: date().week, year: date().year})
+    MERGE (log)-[:HAS_SERVICE_AGGREGATE]->(aggregate)
+
+    WITH ministryStageAttendanceRecord, aggregate, SUM(ministryStageAttendanceRecord.attendance) AS attendance, SUM(aggregate.attendance) AS aggregateAttendance
+    MATCH (aggregate)
+    SET aggregate.attendance = aggregateAttendance + attendance
+    
+    RETURN ministryStageAttendanceRecord
 `
 
 export const recordCancelledService = `
@@ -361,4 +421,23 @@ export const aggregateHubRehearsalDataForCreativeArts = `
         aggregate.numberOfServices = numberOfServices 
 
     RETURN creativearts, aggregate
+`
+export const aggregateStageAttendanceDataForCreativeArts = `
+    MATCH (ministry:Ministry {id: $churchId})
+    
+    WITH ministry as lowerChurch
+
+    MATCH (lowerChurch)<-[:HAS]-(creativeArts:CreativeArts)
+    MATCH (creativeArts)-[:CURRENT_HISTORY|HAS_SERVICE|HAS*2..6]->(record:MinistryStageAttendanceRecord)-[:SERVICE_HELD_ON]->(date:TimeGraph)
+    WHERE date.date.week = date().week AND date.date.year = date().year AND NOT record:NoService
+    WITH DISTINCT creativeArts, record
+    MATCH (creativeArts)-[:CURRENT_HISTORY]->(log:ServiceLog)
+    MERGE (aggregate:AggregateStageAttendanceRecord {id: date().week + '-' + date().year + '-' + log.id, week: date().week, year: date().year})
+    MERGE (log)-[:HAS_SERVICE_AGGREGATE]->(aggregate)
+    WITH  creativeArts, aggregate, collect(record.id) AS componentServiceIds,COUNT(DISTINCT record) AS numberOfServices, SUM(record.attendance) AS totalAttendance
+        SET aggregate.attendance = totalAttendance,
+        aggregate.componentServiceIds = componentServiceIds,
+        aggregate.numberOfServices = numberOfServices 
+
+    RETURN creativeArts, aggregate
 `
--- api/src/resolvers/services/rehearsal-resolver.ts
+++ api/src/resolvers/services/rehearsal-resolver.ts
@@ -7,14 +7,12 @@ import {
   rearrangeCypherObject,
   throwToSentry,
 } from '../utils/utils'
-import {
-  checkCurrentServiceLog,
-  getCurrency,
-  getHigherChurches,
-  getServantAndChurch as getServantAndChurchCypher,
-} from './service-cypher'
+import { getCurrency, getHigherChurches } from './service-cypher'
 
 import {
+  checkCurrentServiceLog,
+  getServantAndChurch as getServantAndChurchCypher,
+  checkStreamServiceDay,
   recordSundayMinistryAttendance,
   recordHubRehearsalService,
   checkMinistryAttendanceFormFilledThisWeek,
@@ -22,7 +20,10 @@ import {
   aggregateMinistryMeetingDataForCreativeArts,
   aggregateMinistryMeetingDataForMinistry,
   aggregateMinistryMeetingDataForHubCouncil,
+  recordOnStageAttendance,
+  checkMinistryStageAttendanceFormFilledThisWeek,
 } from './rehearsal-cypher'
+
 import { SontaHigherChurches } from '../utils/types'
 import { getServiceSontaHigherChurches } from './service-utils'
 
@@ -38,6 +39,13 @@ type RecordServiceArgs = {
   treasurers: string[]
   treasurerSelfie: string
   familyPicture: string
+}
+
+type RecordStageAttendanceArgs = {
+  churchId: string
+  serviceDate: string
+  attendance: number
+  onStagePictures: string[]
 }
 
 export const checkServantHasCurrentHistory = async (
@@ -78,7 +86,7 @@ export const checkServantHasCurrentHistory = async (
   }
 }
 
-const HubFellowshipServiceMutation = {
+const SontaServiceMutation = {
   RecordHubFellowshipSundayAttendance: async (
     object: any,
     args: RecordServiceArgs,
@@ -140,6 +148,7 @@ const HubFellowshipServiceMutation = {
 
     return serviceDetails.ministryAttendanceRecord.properties
   },
+
   RecordHubRehearsalService: async (
     object: any,
     args: RecordServiceArgs,
@@ -232,6 +241,94 @@ const HubFellowshipServiceMutation = {
     }
     return null
   },
+
+  RecordMinistryOnStageAttendance: async (
+    object: any,
+    args: RecordStageAttendanceArgs,
+    context: Context
+  ) => {
+    isAuth(permitLeaderAdmin('Ministry'), context.auth.roles)
+    const session = context.executionContext.session()
+    const sessionTwo = context.executionContext.session()
+    const sessionThree = context.executionContext.session()
+    try {
+      await checkServantHasCurrentHistory(session, context, {
+        churchId: args.churchId,
+      })
+
+      const promises = [
+        session.executeRead((tx) =>
+          tx.run(checkMinistryStageAttendanceFormFilledThisWeek, args)
+        ),
+        sessionTwo.executeRead((tx) => tx.run(getHigherChurches, args)),
+        sessionThree.executeRead((tx) => tx.run(checkStreamServiceDay, args)),
+      ]
+
+      const serviceCheckRes = await Promise.all(promises)
+
+      const serviceCheck = rearrangeCypherObject(serviceCheckRes[0])
+
+      const higherChurches = getServiceSontaHigherChurches(
+        serviceCheckRes[1]?.records
+      ) as SontaHigherChurches
+
+      const streamServiceDayCheck = rearrangeCypherObject(serviceCheckRes[2])
+
+      if (!streamServiceDayCheck.serviceDay) {
+        throw new Error(errorMessage.non_stream_service_day)
+      }
+
+      if (
+        serviceCheck.alreadyFilled &&
+        !['CreativeArts'].some((label) => serviceCheck.labels?.includes(label))
+      ) {
+        throw new Error(errorMessage.no_double_form_filling)
+      }
+
+      if (serviceCheck.labels?.includes('Vacation')) {
+        throw new Error(errorMessage.vacation_cannot_fill_service)
+      }
+
+      let aggregateCypher = ''
+
+      if (higherChurches?.creativeArts) {
+        aggregateCypher =
+          higherChurches.creativeArts.ministryStagePerformanceCypher
+      }
+
+      const cypherResponse = await session
+        .run(recordOnStageAttendance, {
+          ...args,
+          auth: context.auth,
+        })
+        .catch((error: any) =>
+          throwToSentry('Error Recording OnStage Performance attendance', error)
+        )
+
+      const aggregatePromises = [
+        sessionTwo.executeWrite((tx) =>
+          tx.run(aggregateCypher, {
+            churchId: args.churchId,
+          })
+        ),
+      ]
+
+      await Promise.all(aggregatePromises).catch((error: any) =>
+        throwToSentry('Error Aggregating OnStage Performance', error)
+      )
+
+      const serviceDetails = rearrangeCypherObject(cypherResponse)
+
+      return serviceDetails.ministryStageAttendanceRecord.properties
+    } catch (error) {
+      throwToSentry('Error recording OnStage attendance', error)
+    } finally {
+      await session.close()
+      await sessionTwo.close()
+      await sessionThree.close()
+    }
+    return null
+  },
 }
 
-export default HubFellowshipServiceMutation
+export default SontaServiceMutation
