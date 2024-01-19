a7864d72e0c4bbc6a435c1a5ca54ba6cc86a0fbb
Nathaniel Asiak <nathaniel.asiak@acity.edu.gh>
2024-01-19 22:37:03 +0000

    feat: update handler function

--- api/src/functions/update-g-sheets/update-g-sheets.js
+++ api/src/functions/update-g-sheets/update-g-sheets.js
@@ -4,9 +4,7 @@ const { default: axios } = require('axios')
 const { loadSecrets } = require('./secrets.js')
 const {google} = require('googleapis');
 
-
 const SECRETS = loadSecrets()
-
 
 const fetchData = `
 MATCH (gs:Campus {name: $campusName})-[:HAS*2]->(council:Council)<-[:LEADS]-(pastor:Member)
@@ -18,10 +16,9 @@ WHERE record.noServiceReason IS NULL
       MATCH (record)<-[:HAS_SERVICE]-(:ServiceLog)-[:HAS_HISTORY]-(church) WHERE church:Fellowship OR church:Constituency OR church:Council
       MATCH (church)<-[:LEADS]-(leader:Member)
 RETURN DISTINCT date.date.week AS week,date.date AS date, pastor.firstName, pastor.lastName,church.name AS churchName, leader.firstName, 
-leader.lastName, labels(church), record.attendance AS attendance, record.income AS 'Income Not Banked' ORDER BY pastor.firstName,
+leader.lastName, labels(church), record.attendance AS attendance, record.income AS \`Income Not Banked\` ORDER BY pastor.firstName,
 pastor.lastName, date.date.week
 `
-
 const executeQuery = async (neoDriver) => {
   const session = neoDriver.session()
 
@@ -48,13 +45,13 @@ const executeQuery = async (neoDriver) => {
       })
     })
   } catch (error) {
-    console.error('Error copying data from the DB', error)
+    console.error('Error reading data from the DB', error)
   } finally {
     await session.close()
   }
 }
 
-const spreadsheetId = 'YOUR_SPREADSHEET_ID'; // Replace with your Spreadsheet ID
+const spreadsheetId = '1TOlv3QYqLD3SFgYBBuu1r6cCiw-P6m2luxm7Z5ughF0'; // Replace with your Spreadsheet ID
 const google_auth = new google.auth.GoogleAuth({
   keyFile: 'credentials.json', // Path to your Google service account key
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
@@ -63,21 +60,21 @@ const google_auth = new google.auth.GoogleAuth({
 
 const writeToGsheet = async(data, sheetName) => {
   const auth = await google_auth.getClient();
-  const sheets = google.sheets({ version: 'v4', auth: client });
+  const sheets = google.sheets({ version: 'v4', auth: auth });
 
   try {
-   const response = await sheets.spreadsheets.values.append({
-     spreadsheetId,
-     range: `${sheetName}!A1`,
-     requestBody: {values: data}
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range: `${sheetName}!A1`,
+      requestBody: {values: data}
    });
-
- } catch (error) {
-   console.error('Error creating new spreadsheet:', error);
-   throw error;
- }
+  } catch (error) {
+    console.error('Error adding data to google sheet:', error);
+    throw error;
+  }
 
 }
+
 const initializeDatabase = (driver) => {
   return executeQuery(driver).catch((error) => {
     console.error('Database query failed to complete\n', error.message)
@@ -108,9 +105,20 @@ const handler = async () => {
    * have occurred
    */
 
-  await init(driver).catch((error) => {
+  const data = await init(driver).catch((error) => {
     throw new Error(
       `Database initialization failed\n${error.message}\n${error.stack}`
+    )
+  })
+
+  /*
+   * We catch any errors that occur during initialization of the google client
+   * In this case, ensure that the google client is authentication occurs
+  */
+  const sheetName = 'Accra Services'
+  await writeToGsheet(data, sheetName).catch((error) => {
+    throw new Error(
+      `Error writing to google sheet\n${error.message}\n${error.stack}`
     )
   })
 
