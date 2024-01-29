97a6193b59100b9e21ebb28f0b4e250ab45a4719
John-Dag Addy <jaedagy@gmail.com>
2024-01-29 10:23:34 +0000

    feat: add servicesNotBankedQuery and writeToGsheet functions

--- api/src/functions/update-g-sheets/update-g-sheets.js
+++ api/src/functions/update-g-sheets/update-g-sheets.js
@@ -1,94 +1,8 @@
 const neo4j = require('neo4j-driver')
 const { schedule } = require('@netlify/functions')
-const { google } = require('googleapis')
-const { GOOGLE_APPLICATION_CREDENTIALS, SECRETS } = require('./gsecrets.js')
-const { servicesNotBankedThisWeek } = require('./cypher.js')
-
-const executeQuery = async (neoDriver) => {
-  const session = neoDriver.session()
-
-  try {
-    console.log('Running function on date', new Date().toISOString())
-
-    const result = await session.executeRead(async (tx) =>
-      tx.run(servicesNotBankedThisWeek, {
-        campusName: 'Accra',
-      })
-    )
-
-    const headerRow = [
-      'Week',
-      'Date',
-      'Pastor First Name',
-      'Pastor Last Name',
-      'Church Name',
-      'Leader First Name',
-      'Leader Last Name',
-      'Labels',
-      'Attendance',
-      'NotBanked',
-    ]
-
-    const returnValues = [
-      headerRow,
-      ...result.records.map((record) => [
-        record.get('week'),
-        record.get('date'),
-        record.get('pastor.firstName'),
-        record.get('pastor.lastName'),
-        record.get('churchName'),
-        record.get('leader.firstName'),
-        record.get('leader.lastName'),
-        record.get('labels(church)').toString(),
-        record.get('attendance'),
-        record.get('NotBanked'),
-      ]),
-    ]
-
-    return returnValues
-  } catch (error) {
-    console.error('Error reading data from the DB', error)
-  } finally {
-    await session.close()
-  }
-
-  return []
-}
-
-const SPREADSHEET_ID = '1s7jxlEIuerZ8hNPmzVAAhggQAD6LToqSLj0Sd9oU1qY'
-const googleAuth = new google.auth.GoogleAuth({
-  credentials: GOOGLE_APPLICATION_CREDENTIALS,
-  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-})
-
-const writeToGsheet = async (data, sheetName) => {
-  const auth = await googleAuth.getClient()
-  const sheets = google.sheets({ version: 'v4', auth })
-
-  try {
-    await sheets.spreadsheets.values.clear({
-      spreadsheetId: SPREADSHEET_ID,
-      range: `${sheetName}!A:Z`,
-    })
-
-    const response = await sheets.spreadsheets.values.append({
-      spreadsheetId: SPREADSHEET_ID,
-      range: `${sheetName}!A1`,
-      valueInputOption: 'USER_ENTERED',
-      requestBody: { values: data },
-    })
-
-    console.log('Response from google sheets:', response.data)
-  } catch (error) {
-    console.error('Error adding data to google sheet:', error)
-    throw error
-  }
-}
-
-const initializeDatabase = (driver) =>
-  executeQuery(driver).catch((error) => {
-    console.error('Database query failed to complete\n', error.message)
-  })
+const { SECRETS } = require('./gsecrets.js')
+const { writeToGsheet } = require('./utils/writeToGSheet.js')
+const { servicesNotBankedQuery } = require('./query-exec/servicesNotBanked.js')
 
 const handler = async () => {
   const driver = neo4j.driver(
@@ -99,25 +13,24 @@ const handler = async () => {
     )
   )
 
-  const init = async (neoDriver) => initializeDatabase(neoDriver)
+  //! Services Not Banked
+  const servicesNotBankedData = await servicesNotBankedQuery(driver).catch(
+    (error) => {
+      console.error('Database query failed to complete\n', error.message)
+    }
+  )
 
-  const data = await init(driver).catch((error) => {
-    throw new Error(
-      `Database initialization failed\n${error.message}\n${error.stack}`
-    )
-  })
+  const servicesNotBanked = 'Accra Services'
 
-  /*
-   * We catch any errors that occur during initialization of the google client
-   * In this case, ensure that the google client is authentication occurs
-   */
-  const sheetName = 'Accra Services'
+  await writeToGsheet(servicesNotBankedData, servicesNotBanked).catch(
+    (error) => {
+      throw new Error(
+        `Error writing to google sheet\n${error.message}\n${error.stack}`
+      )
+    }
+  )
 
-  await writeToGsheet(data, sheetName).catch((error) => {
-    throw new Error(
-      `Error writing to google sheet\n${error.message}\n${error.stack}`
-    )
-  })
+  //! Outside Accra Weekly Data Update
 
   return {
     statusCode: 200,
