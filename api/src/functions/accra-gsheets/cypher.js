ef436b814c29491fdc8ced9700f4b3a2793d8382
John-Dag Addy <jaedagy@gmail.com>
2024-02-07 12:57:48 +0000

    fix: fix roundning error in cypher

--- api/src/functions/accra-gsheets/cypher.js
+++ api/src/functions/accra-gsheets/cypher.js
@@ -78,7 +78,7 @@ export const weekdayIncomeAttendanceQuery = `
 MATCH (gs:Campus {name: $campusName})-[:HAS*2]->(council:Council)<-[:LEADS]-(pastor:Member)
 OPTIONAL MATCH (council)-[:HAS_HISTORY|HAS_SERVICE|HAS*2..5]->(record:ServiceRecord)-[:SERVICE_HELD_ON]->(date:TimeGraph)
 WHERE date.date.year = date($bussingDate).year AND date.date.week = date($bussingDate).week
-RETURN  pastor.firstName, pastor.lastName,SUM(record.attendance) AS attendance, SUM(round(record.income)) AS income ORDER BY pastor.firstName, pastor.lastName
+RETURN  pastor.firstName, pastor.lastName,SUM(record.attendance) AS attendance, SUM(round(record.income,2)) AS income ORDER BY pastor.firstName, pastor.lastName
 `
 
 export const amountNotBankedQuery = `
