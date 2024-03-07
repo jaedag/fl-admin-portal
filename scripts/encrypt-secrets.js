1bd6ab046a309954a703516dab808591899d1be3
John-Dag Addy <jaedagy@gmail.com>
2024-03-07 09:10:16 +0000

    fix: add payment background function and secrets encryption

--- scripts/encrypt-secrets.js
+++ scripts/encrypt-secrets.js
@@ -5,7 +5,9 @@ async function main() {
   const payload = await secrets.providers.doppler.fetch()
   secrets.build(payload, { path: 'lib/secrets.js' })
   secrets.build(payload, { path: 'api/src/resolvers/secrets.ts' })
-  secrets.build(payload, { path: 'api/src/functions/payment/secrets.js' })
+  secrets.build(payload, {
+    path: 'api/src/functions/payment-background/secrets.js',
+  })
   secrets.build(payload, {
     path: 'api/src/functions/code-of-the-day/secrets.js',
   })
