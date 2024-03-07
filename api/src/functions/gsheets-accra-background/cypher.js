b307d7c9606da2dcb8f2d6dbf542572ee6d5e2ee
John-Dag Addy <jaedagy@gmail.com>
2024-03-07 09:39:26 +0000

    refactor:  anagkazoAmountNotBankedQuery in cypher.js

--- api/src/functions/gsheets-accra-background/cypher.js
+++ api/src/functions/gsheets-accra-background/cypher.js
@@ -120,14 +120,28 @@ RETURN amartey.firstName, amartey.lastName, totalAttendance - donaldAttendance a
 `
 
 export const anagkazoAmountNotBankedQuery = `
-MATCH (gs:Campus {name: $campusName})-[:HAS]->(stream:Stream)-[:HAS]->(council:Council)<-[:LEADS]-(pastor:Member {lastName: "Amartey"})
+MATCH (gs:Campus {name: $campusName})-[:HAS]->(stream:Stream)-[:HAS]->(council:Council)<-[:LEADS]-(pastor:Member {lastName: "Amartey"}) 
 MATCH (stream)-[:HAS_HISTORY|HAS_SERVICE|HAS*2..6]->(record:ServiceRecord)-[:SERVICE_HELD_ON]->(date:TimeGraph)
 WHERE date.date.year = date($bussingDate).year AND date.date.week = date($bussingDate).week
-          AND record.noServiceReason IS NULL
+        AND record.noServiceReason IS NULL
+        AND record.bankingSlip IS NULL
+        AND (record.transactionStatus IS NULL OR record.transactionStatus <> 'success')
+        AND record.tellerConfirmationTime IS NULL
+WITH DISTINCT record, pastor
+WITH  pastor AS amartey,SUM(record.attendance) AS totalAttendance,SUM(round(record.income,2)) AS totalIncome
+
+MATCH (council:Council)<-[:LEADS]-(donald:Member {lastName: "Penney"})
+MATCH (council)-[:HAS_HISTORY|HAS_SERVICE|HAS*2..5]->(record:ServiceRecord)-[:SERVICE_HELD_ON]->(date:TimeGraph)
+        WHERE date.date.year = date($bussingDate).year AND date.date.week = date($bussingDate).week
+        AND record.noServiceReason IS NULL
           AND record.bankingSlip IS NULL
+          AND (record.transactionStatus IS NULL OR record.transactionStatus <> 'success')
           AND record.tellerConfirmationTime IS NULL
-          WITH DISTINCT record, pastor
-RETURN pastor.firstName, pastor.lastName,SUM(round(record.income,2)) AS notBanked ORDER BY pastor.firstName, pastor.lastName
+
+WITH amartey, totalAttendance, SUM(record.attendance) AS donaldAttendance, totalIncome,SUM(round(record.income,2)) AS donaldIncome
+
+
+RETURN amartey.firstName, amartey.lastName,SUM(round(totalIncome,2)) - donaldIncome AS notBanked ORDER BY amartey.firstName, amartey.lastName
 `
 
 export const anagkazoAmountBankedQuery = `
