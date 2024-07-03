4f3eb99d56eaa9e961b459e4c4ac8668b0a6f2cd
John-Dag Addy <jaedagy@gmail.com>
2024-07-03 13:41:31 +0000

    fix: update lastMonth calculation in hillary-monthly-background

--- api/src/functions/hillary-monthly-background/utils/constants.js
+++ api/src/functions/hillary-monthly-background/utils/constants.js
@@ -1,4 +1,4 @@
 export const notifyBaseURL =
   'https://flc-microservices.netlify.app/.netlify/functions/notify'
 
-export const lastMonth = (new Date().getMonth() + 11) % 12
+export const lastMonth = ((new Date().getMonth() + 11) % 12) + 1
