56cb7259de026dc92cf287807915b0988f722a8c
John-Dag Addy <jaedagy@gmail.com>
2024-07-15 14:11:07 +0000

    refactor: handle network errors in AppWithApollo component

--- web-react-ts/src/index.tsx
+++ web-react-ts/src/index.tsx
@@ -125,27 +125,31 @@ const AppWithApollo = () => {
       )
     }
 
-    // if (networkError)
-    //   enqueueSnackbar(
-    //     <Card>
-    //       <Card.Header className="fw-bold">Network Error</Card.Header>
-    //       <Card.Body>
-    //         <div>{`Message: ${networkError?.message}`}</div>
-    //         {/* <div>{`Stack: ${JSON.stringify(networkError?.stack)}`}</div> */}
-    //       </Card.Body>
-    //     </Card>,
-    //     {
-    //       action,
-    //       preventDuplicate: true,
-    //       variant: 'error',
-    //       autoHideDuration: 20000,
-    //       hideIconVariant: true,
-    //       anchorOrigin: {
-    //         vertical: 'bottom',
-    //         horizontal: 'right',
-    //       },
-    //     }
-    //   )
+    if (
+      networkError &&
+      !networkError.message.includes('400') &&
+      !networkError.message.includes('Failed to fetch')
+    )
+      enqueueSnackbar(
+        <Card>
+          <Card.Header className="fw-bold">Network Error</Card.Header>
+          <Card.Body>
+            <div>{`Message: ${networkError?.message}`}</div>
+            {/* <div>{`Stack: ${JSON.stringify(networkError?.stack)}`}</div> */}
+          </Card.Body>
+        </Card>,
+        {
+          action,
+          preventDuplicate: true,
+          variant: 'error',
+          autoHideDuration: 20000,
+          hideIconVariant: true,
+          anchorOrigin: {
+            vertical: 'bottom',
+            horizontal: 'right',
+          },
+        }
+      )
   })
 
   const errorPolicy = 'all'
