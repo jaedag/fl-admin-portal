94306252b5ad7a9f2b5826f1e5c9b1583b783cff
John-Dag Addy <jaedagy@gmail.com>
2024-08-10 17:47:32 +0000

    refactor: update link to service details page in BankingDefaulters

--- web-react-ts/src/pages/services/defaulters/Banked.tsx
+++ web-react-ts/src/pages/services/defaulters/Banked.tsx
@@ -85,7 +85,7 @@ const Banked = () => {
               <Col key={i} xs={12} className="mb-3">
                 <DefaulterCard
                   defaulter={defaulter}
-                  link="/fellowship/service-details"
+                  link="/bacenta/service-details"
                 />
               </Col>
             ))}
