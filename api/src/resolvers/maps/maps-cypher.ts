62d35ca87dee1e5a4900a344fa1028edadfcd7dd
John-Dag Addy <jaedagy@gmail.com>
2024-08-21 00:23:17 +0000

    refactor: update member search queries to use Bacenta instead of Fellowship

--- api/src/resolvers/maps/maps-cypher.ts
+++ api/src/resolvers/maps/maps-cypher.ts
@@ -1,9 +1,9 @@
-export const memberFellowshipSearchByName = `
-MATCH (this:Member {id: $id})-[:LEADS|HAS|IS_ADMIN_FOR*1..6]->(fellowship:Active:Fellowship)<-[:LEADS]-(fellowshipLeader:Member)
-MATCH (fellowship)<-[:HAS]-(:Bacenta)<-[:HAS]-(:Constituency)<-[:HAS]-(council:Council)<-[:LEADS]-(councilLeader:Member)
-WHERE toLower(fellowship.name) CONTAINS toLower($key) AND fellowship.location IS NOT NULL
-RETURN DISTINCT fellowship,
-fellowshipLeader {
+export const memberBacentaSearchByName = `
+MATCH (this:Member {id: $id})-[:LEADS|HAS|IS_ADMIN_FOR*1..5]->(bacenta:Active:Bacenta)<-[:LEADS]-(bacentaLeader:Member)
+MATCH (bacenta)<-[:HAS]-(:Constituency)<-[:HAS]-(council:Council)<-[:LEADS]-(councilLeader:Member)
+WHERE toLower(bacenta.name) CONTAINS toLower($key) AND bacenta.location IS NOT NULL
+RETURN DISTINCT bacenta,
+bacentaLeader {
   .id,
   .firstName,
   .lastName,
@@ -27,7 +27,7 @@ LIMIT toInteger($limit)
 `
 
 export const memberLoadCouncilUnvisitedMembers = `
-MATCH (this:Member {id: $id})-[:LEADS|IS_ADMIN_FOR]->(council:Council)-[:HAS]->(:Constituency)-[:HAS]->(:Bacenta)-[:HAS]->(:Fellowship)<-[:BELONGS_TO]-(member:Member)-[:LEADS|IS_ADMIN_FOR]->()
+MATCH (this:Member {id: $id})-[:LEADS|IS_ADMIN_FOR]->(council:Council)-[:HAS]->(:Constituency)-[:HAS]->(:Bacenta)<-[:BELONGS_TO]-(member:Member)-[:LEADS|IS_ADMIN_FOR]->()
 MATCH (cycle:CouncilCycle {half: toInteger(ceil(toFloat(date().month)/toFloat(6))) - 1, year: date().year})
 MATCH (author:Member {auth_id: $auth.jwt.sub})-[:CURRENT_HISTORY]->(log:ServiceLog)<-[:CURRENT_HISTORY]-(council)
 WHERE NOT EXISTS {
@@ -35,17 +35,17 @@ WHERE NOT EXISTS {
    WHERE (visitation)-[:TOWARDS]->(member) AND (visitation)-[:DURING_CYCLE]->(cycle)
 }
 
-OPTIONAL MATCH (author)-[:LEADS]->(lowerChurch)-[:HAS*0..]->(:Bacenta)<-[:BELONGS_TO]-(alreadyMember:Member)
-WHERE lowerChurch:Constituency OR lowerChurch:Bacenta OR lowerChurch:Fellowship
+OPTIONAL MATCH (author)-[:LEADS]->(lowerChurch)<-[:BELONGS_TO]-(alreadyMember:Member)
+WHERE lowerChurch:Constituency OR lowerChurch:Bacenta
 WITH member, author, collect(DISTINCT alreadyMember) AS alreadyMembers WHERE NOT member IN alreadyMembers
 WITH member, author WHERE member <> author
 AND member.location IS NOT NULL
 
 WITH DISTINCT member
-MATCH (member)-[:BELONGS_TO]->(fellowship:Fellowship)<-[:HAS*3]-(council:Council)<-[:LEADS]-(pastor:Member)
+MATCH (member)-[:BELONGS_TO]->(bacenta:Bacenta)<-[:HAS*2]-(council:Council)<-[:LEADS]-(pastor:Member)
 
   RETURN DISTINCT member, 
-  fellowship {
+  bacenta {
     .id,
     .name,
     .location
@@ -66,13 +66,13 @@ MATCH (member)-[:BELONGS_TO]->(fellowship:Fellowship)<-[:HAS*3]-(council:Council
 `
 
 export const memberMemberSearchByName = `
-MATCH (this:Member {id: $id})-[:LEADS|HAS|IS_ADMIN_FOR*1..7]->(:Fellowship)<-[:BELONGS_TO]-(member:Active:Member)
+MATCH (this:Member {id: $id})-[:LEADS|HAS|IS_ADMIN_FOR*1..6]->(:Bacenta)<-[:BELONGS_TO]-(member:Active:Member)
 WHERE toLower(member.firstName+ ' ' + member.lastName) CONTAINS toLower($key) AND member.location IS NOT NULL
 WITH DISTINCT member
-MATCH (member)-[:BELONGS_TO]->(fellowship:Fellowship)<-[:HAS*3]-(council:Council)<-[:LEADS]-(pastor:Member)
+MATCH (member)-[:BELONGS_TO]->(bacenta:Bacenta)<-[:HAS*2]-(council:Council)<-[:LEADS]-(pastor:Member)
 
   RETURN DISTINCT member, 
-  fellowship {
+  bacenta {
     .id,
     .name,
     .location
@@ -104,13 +104,13 @@ WHERE toLower(outreachVenue.name) CONTAINS toLower($key) AND outreachVenue.locat
 RETURN DISTINCT outreachVenue LIMIT toInteger($limit)
 `
 
-export const memberFellowshipSearchByLocation = `
-  MATCH (this:Member {id: $id})-[:LEADS|HAS|IS_ADMIN_FOR*1..6]->(fellowship:Fellowship)<-[:LEADS]-(fellowshipLeader:Member)
-  MATCH (fellowship)<-[:HAS]-(:Bacenta)<-[:HAS]-(:Constituency)<-[:HAS]-(council:Council)<-[:LEADS]-(councilLeader:Member)
-  WITH fellowship, fellowshipLeader, council, councilLeader, point.distance(point({latitude: fellowship.location.latitude, longitude: fellowship.location.longitude}), point({latitude: $latitude, longitude: $longitude})) AS distance
+export const memberBacentaSearchByLocation = `
+  MATCH (this:Member {id: $id})-[:LEADS|HAS|IS_ADMIN_FOR*1..5]->(bacenta:Bacenta)<-[:LEADS]-(bacentaLeader:Member)
+  MATCH (bacenta)<-[:HAS]-(:Bacenta)<-[:HAS]-(:Constituency)<-[:HAS]-(council:Council)<-[:LEADS]-(councilLeader:Member)
+  WITH bacenta, bacentaLeader, council, councilLeader, point.distance(point({latitude: bacenta.location.latitude, longitude: bacenta.location.longitude}), point({latitude: $latitude, longitude: $longitude})) AS distance
   WHERE distance <= 5000
-  RETURN DISTINCT fellowship,
-  fellowshipLeader {
+  RETURN DISTINCT bacenta,
+  bacentaLeader {
     .id,
     .firstName,
     .lastName,
@@ -129,7 +129,7 @@ export const memberFellowshipSearchByLocation = `
     .phoneNumber,
     .whatsappNumber,
     .pictureUrl
-  }, distance ORDER BY distance, fellowship.name ASC LIMIT 30
+  }, distance ORDER BY distance, bacenta.name ASC LIMIT 30
 `
 
 export const memberMemberSearchByLocation = `
@@ -138,10 +138,10 @@ export const memberMemberSearchByLocation = `
   WHERE distance <= 5000
 
 WITH DISTINCT member, distance
-MATCH (member)-[:BELONGS_TO]->(fellowship:Fellowship)<-[:HAS*3]-(council:Council)<-[:LEADS]-(pastor:Member)
+MATCH (member)-[:BELONGS_TO]->(bacenta:Bacenta)<-[:HAS*2]-(council:Council)<-[:LEADS]-(pastor:Member)
 
 RETURN DISTINCT member, 
-  fellowship {
+  bacenta {
     .id,
     .name,
     .location
