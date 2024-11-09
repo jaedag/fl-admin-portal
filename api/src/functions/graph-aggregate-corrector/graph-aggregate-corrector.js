472b162b7ce2661b12222a3e07d2df8278a86663
John-Dag <jaedagy@gmail.com>
2024-11-09 05:47:39 +0000

    feat: add console log for aggregation completion in graph-aggregate-corrector

--- api/src/functions/graph-aggregate-corrector/graph-aggregate-corrector.js
+++ api/src/functions/graph-aggregate-corrector/graph-aggregate-corrector.js
@@ -24,6 +24,7 @@ const executeQuery = async (neoDriver) => {
       aggregateCampusOnOversight(neoDriver),
       aggregateOversightOnDenomination(neoDriver),
     ])
+    console.log('Aggregation complete')
   } catch (error) {
     console.error('Error aggregating graphs', error)
   } finally {
